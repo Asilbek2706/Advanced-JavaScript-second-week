89b08df9ffcf560fc378ecc5d14992b4930333d6
Asilbek2706 <asilbekkaromatov2@gmail.com>
2026-01-05 14:19:49 +0500

    Event Propagation

---
+++ second/event.js
@@ -0,0 +1,37 @@
+// Event Propagation
+
+// const grandParent = document.getElementById("grandparent");
+// const parent = document.getElementById("parent");
+// const child = document.getElementById("child");
+// Bubbling
+// document.body.addEventListener("click", () => {
+//     alert("Body clicked");
+// })
+//
+// grandParent.addEventListener("click", function (event) {
+//     alert("Grand parent clicked");
+// });
+//
+// parent.addEventListener("click", function (event) {
+//     alert("Parent clicked");
+// });
+// child.addEventListener("click", function (event) {
+//     alert("Children clicked");
+// })
+
+
+const grandParent = document.querySelector("#grandparent");
+const parent = document.querySelector("#parent");
+const child = document.querySelector("#child");
+//Capturing
+
+grandParent.addEventListener("click", function (event) {
+    alert("Grand parent clicked");
+}, true);
+
+parent.addEventListener("click", function (event) {
+    alert("Parent clicked");
+}, true);
+child.addEventListener("click", function (event) {
+    alert("Children clicked");
+}, true)
